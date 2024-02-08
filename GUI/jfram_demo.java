382bb63e5c760cd3f2994c0b80323bff74478c2d
Hamid Raza <74106196+razahamid91@users.noreply.github.com>
2024-02-08 10:14:28 +0530

    k

--- GUI/jfram_demo.java
+++ GUI/jfram_demo.java
@@ -1,16 +1,23 @@
 package GUI;
+
 import javax.swing.*;
 
 public class jfram_demo {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        JFrame jf = new JFrame();
+        JFrame jf = new JFrame("demo");
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jf.setVisible(true);
         // jf.setSize(800, 600);
         // jf.setLocation(500,300);
-        jf.setBounds(500,300,800,600); // for set size as well location is used setBounds   
+        jf.setBounds(500, 300, 800, 600); // for set size as well location is used setBounds
+
+        // jf.setTitle("my first application of gui"); // for set title
+
+        ImageIcon icon = new ImageIcon("./images.jpg");
+        jf.setIconImage(icon.getImage());
+
     }
-    
+
 }
